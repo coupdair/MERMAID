994a1dacc94ec1c4e719ba12b36d776f09d2fadb
temporary <temp@elphel.univ-lille1.fr>
2015-02-24 20:01:11 +0100

    2015_02_24_20h00min

--- data/sync.sh
+++ data/sync.sh
@@ -1,6 +1,7 @@
 #sync data to GitHub
+date=`date +%Y_%m_%d_%Hh%Mmin`
 cd /tmp/MERMAID/data
 for i in 101 115 175 130 126 110 128; do rsync -var pi@172.16.0.$i:~/data.Xsensor/2015 . --progress; done
 git add *
-git commit -a -m 'update'
+git commit -a -m $date
 git push
